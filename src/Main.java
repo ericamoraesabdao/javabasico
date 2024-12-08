df9964b30056cd1b78fde7ab6d92975347103dbf
Erica <erica.abdao@zup.com.br>
2024-12-08 13:48:53 -0300

    Adicionar exercício número 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
-
 public class Main {
     public static void main(String[] args) {
+        class Exercicio1 extends SomaDeNumeros {
+ //           System.out.println(Exercicio1);
+        }
 
     }
 }
--- src/SomaDeNumeros.java
+++ src/SomaDeNumeros.java
@@ -15,7 +15,7 @@ public class SomaDeNumeros {
         n = scanner.nextInt();
 
         for (int i = 0; i <= n; i++) {
-            soma = soma + i;
+            soma = soma +i;
 
         }
 
