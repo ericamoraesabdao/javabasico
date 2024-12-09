9776eb0ece13c53f8153ef93992c3b8861c44bfc
Erica <erica.abdao@zup.com.br>
2024-12-09 18:30:06 -0300

    Adicionar exercício número 5.

---
+++ src/Potencia.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Potencia {
+    //6 - Potência
+    //● Descrição: Escreva um programa que calcule a potência de um número base
+    //elevado a expoente usando um loop for.
+    //● Exemplo de entrada: 2 (base), 3 (expoente)
+    //● Exemplo de saída: 8 (2^3)
+
+    public static void main(String[] args) {
+        int base;
+        int expoente;
+        int resultado = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a base: ");
+        base = scanner.nextInt();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Digite o expoente: ");
+        expoente = scanner2.nextInt();
+
+        for (int i = 1; i <= expoente ; i++) {
+           resultado = resultado * base;
+        }
+        System.out.println(resultado);
+    }
+}
